e27ec7782f084536caa1ae3fb3eb547ff9507208
tzahid0297 <76126972+tzahid0297@users.noreply.github.com>
2021-03-23 19:55:12 +0100

    Moved jpeg to src folder and removed svg anf removed their code

--- src/Otherdata.js
+++ src/Otherdata.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "./Otherdata.css";
-import humidity from "./humidity.svg";
-import wind from "./wind.svg";
+;
 
 export default function Otherdata() {
   return (
@@ -9,11 +8,11 @@ export default function Otherdata() {
       <div className="row">
         <div className="col otherData">
           <p>
-            <img src={humidity} alt="Humidity" /> Humidity:
+             Humidity:
             <span id="humidity"> 13</span>%
           </p>
           <p>
-            <img src={wind} alt="Wind" /> Wind:
+             Wind:
             <span id="windspeed"> 20</span>km/h
           </p>
         </div>
