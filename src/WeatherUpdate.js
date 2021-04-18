a87a99d7d55e40c16eb332da4ac6e086717e70d3
tzahid0297 <76126972+tzahid0297@users.noreply.github.com>
2021-04-18 15:30:26 +0200

    Added class for wind and humidity

--- src/WeatherUpdate.js
+++ src/WeatherUpdate.js
@@ -29,13 +29,13 @@ export default function WeatherUpdate(props) {
       <div className="row otherData">
           <div className="col">
           <p>
-             Humidity:{" "}
+            <i class="fas fa-tint"></i> Humidity:{" "}
             <span id="humidity">{props.data.humidity}</span>{" "}%
           </p>
           </div>
-          <div className="col"> 
+          <div className="col">
           <p>
-             Wind:{" "}
+            <i class="fas fa-wind"></i> Wind:{" "} 
             <span id="windspeed">{Math.round(props.data.wind)}</span>{" "}km/h
           </p>
           </div> 
